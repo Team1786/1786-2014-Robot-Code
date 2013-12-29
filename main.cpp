c29cbe9a8d1524b349c4f9263246add789e60079
Adam Goldsmith <adamgoldsmith1217@gmail.com>
2013-12-29 00:54:39 +0000

    Clean things up and rewrite the invert Driving section.

--- main.cpp
+++ main.cpp
@@ -4,59 +4,55 @@ class main : public IterativeRobot
 {
 	RobotDrive drivetrain;
 	Joystick stick;
-	float stickY;
-	float stickX;
-	int invertDriving;
-	bool invertDrivingNext;
-	
+	float rotate;
+	float drive;
+
+private:
+	void updateJoystick(){
+		static bool invertButtonHeld=false;
+		static int invertDrive=1; //1 for normal, -1 for inverted
+		if(stick.GetRawButton(1)!=invertButtonHeld&&stick.GetRawButton(1)) invertDrive=-invertDrive; //if invert button changed and new button state is pressed, invert invertDrive
+		invertButtonHeld=stick.GetRawButton(1); //update stored value for button
+		float throttleScale=((1-stick.GetTwist())/2); //make throttle 0-1 for scaling the joystick input
+		rotate=-stick.GetX()*throttleScale*invertDrive; //get X and multiply by throttleScale and invertDrive //TODO check which of these (X/Y) need to be inverted to begin with
+		drive=stick.GetY()*throttleScale*invertDrive; //do the same with Y
+	}
+
 public:
 	main(void):
 		//init the Joystick and RobotDrive (numbers refer to ports)
 		drivetrain(1,2),
 		stick(1)
 	{
-		invertDriving=1;
 	}
-	
-	void updateJoystick(){
-		stickY=stick.GetY()*((-stick.GetTwist()+1)/2); //get Y and multiply by Twist (which is actually Throttle), after making it 0-1
-		stickX=stick.GetX()*((-stick.GetTwist()+1)/2); //do the same with X
-		if(stick.GetRawButton(1)) invertDrivingNext=true; //check if button pressed and change flag to invert driving when the button is released
-		if(!stick.GetRawButton(1)&&invertDrivingNext) //check if button released and flag set
-		{
-			invertDriving=-invertDriving; //invert driving
-			invertDrivingNext=false; //reset flag so that it doesn't keep switching
-		}
-	}
-	
+
 	void autonomousPeriodic(void)
 	{
 	}
-	
+
 	void TeleopInit(void)
 	{
+		printf("Starting Teleop mode");
 		drivetrain.SetExpiration(2); //set the timeout for the watchdog
 		drivetrain.SetSafetyEnabled(true); //enable watchdog
-		printf("Starting Teleop mode");
 	}
-	
+
 	void TeleopPeriodic(void)
 	{
 		updateJoystick();
-		//printf("X:%f,Y%f\n", stickX, stickY);
-		drivetrain.ArcadeDrive(invertDriving*stickY,-stickX,false); //pass the joystick information to the drivetrain using the simplified ArcadeDrive
+		drivetrain.ArcadeDrive(drive,rotate,false); //pass the joystick information to the drivetrain using the simplified ArcadeDrive
 	}
-	
+
 	void TestInit(void)
 	{
-		printf("Starting Test");
-		drivetrain.SetSafetyEnabled(false); //disable watchdog so that it doesn't fill the log with useless stuff. Also, we don't really want to move here
+		printf("Starting Test mode");
+		drivetrain.SetSafetyEnabled(false); //disable watchdog so that it doesn't fill the log with useless stuff. Also, we don't really want to move in Test
 	}
-	
+
 	void TestPeriodic(void) //echos debugging info through netconsole
 	{
 		updateJoystick();
-		printf("X:%f,Y%f,Z:%f,Twist:%f,Throttle:%f\n", stickX, stickY, stick.GetZ(), stick.GetTwist(), stick.GetThrottle()); //report what WPILIB thinks these are. Some don't match with what we think they are.
+		printf("rotate:%f,drive%f,X:%f,Y:%f,Z(Twist):%f,Twist(Throttle):%f,Throttle:%f\n", rotate, drive, stick.GetX(), stick.GetY(), stick.GetZ(), stick.GetTwist(), stick.GetThrottle()); //report what WPILIB thinks these are. Some don't match with what we think they are.
 	}
 };
 
