602acaf7b47f9b18897d7e51853650f0a27513c4
Adam Goldsmith <adamgoldsmith1217@gmail.com>
2014-01-03 12:22:01 +0000

    Change updateJoystick to not use global variables

--- main.cpp
+++ main.cpp
@@ -4,18 +4,23 @@ class main : public IterativeRobot
 {
 	RobotDrive drivetrain;
 	Joystick stick;
-	float rotate;
-	float drive;
 
 private:
-	void updateJoystick(){
+	struct input
+	{
+		float rotate;
+		float drive;
+	};
+	input updateJoystick(){
+		input js;
 		static bool invertButtonHeld=false;
 		static int invertDrive=1; //1 for normal, -1 for inverted
 		if(stick.GetRawButton(1)!=invertButtonHeld&&stick.GetRawButton(1)) invertDrive=-invertDrive; //if invert button changed and new button state is pressed, invert invertDrive
 		invertButtonHeld=stick.GetRawButton(1); //update stored value for button
 		float throttleScale=((1-stick.GetTwist())/2); //make throttle 0-1 for scaling the joystick input
-		rotate=-stick.GetX()*throttleScale*invertDrive; //get X and multiply by throttleScale and invertDrive //TODO check which of these (X/Y) need to be inverted to begin with
-		drive=stick.GetY()*throttleScale*invertDrive; //do the same with Y
+		js.rotate=-stick.GetX()*throttleScale*invertDrive; //get X and multiply by throttleScale and invertDrive //TODO check which of these (X/Y) need to be inverted to begin with
+		js.drive=stick.GetY()*throttleScale*invertDrive; //do the same with Y
+		return js;
 	}
 
 public:
@@ -39,8 +44,8 @@ public:
 
 	void TeleopPeriodic(void)
 	{
-		updateJoystick();
-		drivetrain.ArcadeDrive(drive,rotate,false); //pass the joystick information to the drivetrain using the simplified ArcadeDrive
+		input js=updateJoystick();
+		drivetrain.ArcadeDrive(js.drive,js.rotate,false); //pass the joystick information to the drivetrain using the WPILib method ArcadeDrive
 	}
 
 	void TestInit(void)
@@ -49,10 +54,10 @@ public:
 		drivetrain.SetSafetyEnabled(false); //disable watchdog so that it doesn't fill the log with useless stuff. Also, we don't really want to move in Test
 	}
 
-	void TestPeriodic(void) //echos debugging info through netconsole
+	void TestPeriodic(void) //prints debugging info to netconsole
 	{
-		updateJoystick();
-		printf("rotate:%f,drive%f,X:%f,Y:%f,Z(Twist):%f,Twist(Throttle):%f,Throttle:%f\n", rotate, drive, stick.GetX(), stick.GetY(), stick.GetZ(), stick.GetTwist(), stick.GetThrottle()); //report what WPILIB thinks these are. Some don't match with what we think they are.
+		input js=updateJoystick();
+		printf("rotate:%f,drive%f,X:%f,Y:%f,Z(Twist):%f,Twist(Throttle):%f,Throttle:%f\n", js.rotate, js.drive, stick.GetX(), stick.GetY(), stick.GetZ(), stick.GetTwist(), stick.GetThrottle()); //report what WPILIB thinks these are. Some don't match with what we think they are.
 	}
 };
 
