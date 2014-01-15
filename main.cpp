d4131bd6e11dbde9c32b3952e67d577ef3221a2c
God <m_spencer@dublinschool.org>
2014-01-14 21:28:14 -0500

    Addtion of Tasks with Resume, Suspend, and Stop with debug statements

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "WPILib.h"
 #include "CRioNetworking.h"
 
+int networkMethod(void);
+
+bool isComm;
+string data;
+
 class main : public IterativeRobot
 {
 	RobotDrive drivetrain;
 	Joystick stick;
+	Task *networking;
 
 private:
 	struct input
@@ -13,7 +19,8 @@ private:
 		float drive;
 	};
 	
-	input updateJoystick(){
+	input updateJoystick()
+	{
 		input js;
 		static bool invertButtonHeld=false;
 		static int invertDrive=1; //1 for normal, -1 for inverted
@@ -31,10 +38,16 @@ public:
 		drivetrain(1,2),
 		stick(1)
 	{
-		
+		networking = new Task("networking", (FUNCPTR)&networkMethod);
+		isComm = false;
+	}
+	
+	void AutonomousInit(void)
+	{
+		networking->Start();
 	}
 
-	void autonomousPeriodic(void)
+	void AutonomousPeriodic(void)
 	{
 		
 	}
@@ -49,7 +62,17 @@ public:
 	void TeleopPeriodic(void)
 	{
 		input js=updateJoystick();
-		drivetrain.ArcadeDrive(js.drive,js.rotate,false); //pass the joystick information to the drivetrain using the WPILib method ArcadeDrive
+		if(!isComm)
+		{
+			drivetrain.ArcadeDrive(js.drive,js.rotate,false); //pass the joystick information to the drivetrain using the WPILib method ArcadeDrive
+		}
+		else
+		{
+			//int c = data.find_first_of(',');
+			//char* drive = data.substr(0, c);
+			//char* rotate = (char*)data.substr(c);
+			//drivetrain.ArcadeDrive(atof((char*)drive), atof((char*)rotate), false); //pass the joystick information to the drivetrain using the WPILib method ArcadeDrive
+		}
 	}
 	
 	void TeleopContinuous(void)
@@ -59,15 +82,58 @@ public:
 
 	void TestInit(void)
 	{
-		printf("Starting Test mode");
+		printf("Starting Test mode\n");
 		drivetrain.SetSafetyEnabled(false); //disable watchdog so that it doesn't fill the log with useless stuff. Also, we don't really want to move in Test
+		networking->Start();
+		printf("network started\n");
 	}
 
 	void TestPeriodic(void) //prints debugging info to netconsole
 	{
 		input js=updateJoystick();
-		printf("rotate:%f,drive%f,X:%f,Y:%f,Z(Twist):%f,Twist(Throttle):%f,Throttle:%f\n", js.rotate, js.drive, stick.GetX(), stick.GetY(), stick.GetZ(), stick.GetTwist(), stick.GetThrottle()); //report what WPILIB thinks these are. Some don't match with what we think they are.
+		//printf("rotate:%f,drive%f,X:%f,Y:%f,Z(Twist):%f,Twist(Throttle):%f,Throttle:%f\n", js.rotate, js.drive, stick.GetX(), stick.GetY(), stick.GetZ(), stick.GetTwist(), stick.GetThrottle()); //report what WPILIB thinks these are. Some don't match with what we think they are.
+		if(stick.GetY() < -.5 && !networking->IsSuspended())
+		{
+			printf("Suspending\n");
+			networking->Suspend();
+		}
+		else if(stick.GetY() > .5 && networking->IsSuspended())
+		{
+			printf("Resuming\n");
+			networking->Resume();
+		}
+	}
+	
+	void DisabledInit(void)
+	{
+		printf("Stopping");
+		networking->Stop();
 	}
 };
+
+int networkMethod(void)
+{
+	while(true)
+	{
+		printf("Task!\n");
+		sleep(1);	
+	}
+	//CRioNetworking* cRio = new CRioNetworking();
+	//cRio->connect();
+	//cRio->send("Ready!");
+	while(true)
+	{
+		if(isComm)
+		{
+			//cRio->send("Give my data!!!");
+			//char* data;
+			while(isComm)
+			{
+				//data = (char*)cRio->receive();
+			}
+		}
+	}
+	return 0;
+}
 
 START_ROBOT_CLASS(main);
